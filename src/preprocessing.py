291651fb85fa665de93f2c7f5e8d4bb377f4225c
l1rn <ivtaks06@gmail.com>
2026-02-16 22:57:36 +0500

    vpd calculation

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,5 +1,15 @@
 from src.data_loader import *
 from src.config import DATA_DIR
+import numpy as np
+
+KELVIN = 273.15
+def calculate_vpd(t2m_k, d2m_k) -> float:
+    t_c = t2m_k - KELVIN
+    d_c = d2m_k - KELVIN
+
+    es = 610.78 * np.exp((17.2694 * t_c) / (t_c + 237.3))
+    ea = 610.78 * np.exp((17.2694 * d_c) / (d_c + 237.3))
+    return es - ea
 
 def process_data():
     """ Data Integration """
@@ -10,21 +20,14 @@ def process_data():
     ds = load_meterological(f"{DATA_DIR}/khmao_era5.nc")
     firms = load_firms(f"{DATA_DIR}/khmao_fire_archive.csv")
     
-    temp = ds["t2m"]
-    temp_month = temp.mean(dim="valid_time")
-    temp_month = temp_month.rio.write_crs("EPSG:4326")
+    t2m_monthly = ds["t2m"].resample(valid_time="1ME").mean()
+    d2p_monthly = ds["d2m"].resample(valid_time="1ME").mean()
+    t2m_monthly = t2m_monthly.rio.write_crs("EPSG:4326")
+    d2p_monthly = d2p_monthly.rio.write_crs("EPSG:4326")
     
-    lc_matched = lc.rio.reproject_match(temp_month)
-    human_mod_matched = human_mod.rio.reproject_match(temp_month)
-    dem_matched = dem.rio.reproject_match(temp_month)
-    
-    print("HMD resolution:", human_mod.rio.resolution())
-    print("LandCover resolution:", lc.rio.resolution())
-    print("DEM resolution:", dem.rio.resolution())
-    print("ERA resolution:", temp.rio.resolution())
-    
-    print("Matched LandCover resolution:", lc_matched.rio.resolution())
-    print("Matched HMD resolution:", human_mod_matched.rio.resolution())
-    print("Matched DEM resolution:", dem_matched.rio.resolution())
-    
-    print("temp_month Shape: ", temp_month.shape)
+    # lc_matched = lc.rio.reproject_match(temp_monthly)
+    # human_mod_matched = human_mod.rio.reproject_match(temp_monthly)
+    # dem_matched = dem.rio.reproject_match(temp_monthly)
+    vpd_monthly = calculate_vpd(t2m_monthly, d2p_monthly)
+    print(vpd_monthly.min().values)
+    print(vpd_monthly.max().values)
